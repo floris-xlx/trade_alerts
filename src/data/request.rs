670a92750b6ffb5fbcd52110f8a93dfe7933a527
Twixan <hadi@xylex.ai>
2024-04-25 22:11:58 +0200

    emm

--- src/data/request.rs
+++ src/data/request.rs
@@ -10,3 +10,19 @@
 
 // ## Implement the XylexApi struct for Request
 //
+
+/*
+use std::error::Error;
+
+use crate::data::XylexApi;
+
+impl XylexApi {
+
+    pub async fn request_real_time_price(&self, symbol: &str) -> Result<(f64), Box<dyn Error + Send + Sync>>{
+        let client = self.client.clone();
+        let response = client.get(format!("{}/v1/market/price?symbol={}", self.api_endpoint, symbol)).send().await?;
+        let price = response.json::<f64>().await?;
+        Ok(price)
+    }
+}
+*/
